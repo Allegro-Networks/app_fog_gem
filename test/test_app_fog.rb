0ecbba2c8f7c155b7b46a799add70fa182378be7
ruby_gem <gemma.cameron@allegro.net>
2013-06-08 08:52:49 +0100

    changed to a global

--- test/test_app_fog.rb
+++ test/test_app_fog.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 
 class TestAppFog < Test::Unit::TestCase
 	def test_login
-		command = 'af login --email username --passwd password'
-		assert_equal 'af login --email username --passwd password', command
+		@command = 'af login --email username --passwd password'
+		assert_equal 'af login --email username --passwd password', @command
 	end
 end
