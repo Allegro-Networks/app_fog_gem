063165b09c7b922f72a000bcc8f2c33d3145cbfb
ruby_gem <gemma.cameron@allegro.net>
2013-06-11 17:15:44 +0100

    trying to fix the paths

--- lib/app_fog.rb
+++ lib/app_fog.rb
@@ -1,3 +1,5 @@
+require 'app_fog/shell_command_wrapper'
+
 class AppFog
 	def initialize(parameters)
 		@shell_command = parameters[:shell_command] || ShellCommandWrapper.new()
@@ -17,34 +19,4 @@ class AppFog
 	def start(app_name)
 		@shell_command.perform "af start #{app_name}"
 	end
-end
-
-class Credentials
-  attr_reader :username, :password
- 
-  def initialize(parameters)
-    @username = parameters[:username]
-    @password = parameters[:password]
-  end
-end
-
-class ShellCommandWrapper
-	def initialize(open3 = Open3)
-		@open3 = open3
-	end
-
-	def perform(command)
-		stdin, stdout, stderr = @open3.popen3 command
-		output = stdout.read unless stdout == nil
-
-		puts output
-
-		if (output != nil && output.include?('Problem with login, invalid account or password when attempting to login to'))
-			raise LoginError.new(output)
-		end
-	end
-end
-
-class LoginError < StandardError
-
 end
