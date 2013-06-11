01f25da81f8c5e76bdbf5f3ea1ef970d81d5370a
ruby_gem <gemma.cameron@allegro.net>
2013-06-11 12:13:25 +0100

    removed the need for credentials

--- test/test_app_fog.rb
+++ test/test_app_fog.rb
@@ -5,22 +5,18 @@ require '../lib/app_fog.rb'
 
 class TestAppFog < Test::Unit::TestCase
 	def test_login
-		credentials = Credentials.new(username: 'username', password: 'password')
-
-		appfog = AppFog.new(credentials, self)
+		appfog = AppFog.new(username: 'username', password: 'password', shell_command: self)
 		assert_equal 'af login --email username --passwd password', @command
 	end
 
 	def test_update
-		credentials = Credentials.new(username: 'username', password: 'password')
-		appfog = AppFog.new(credentials, self)
+		appfog = AppFog.new(username: 'username', password: 'password', shell_command: self)
 		appfog.update('app-name')
 		assert_equal 'af update app-name', @command
 	end
 
 	def test_start
-		credentials = Credentials.new(username: 'username', password: 'password')
-		appfog = AppFog.new(credentials, self)
+		appfog = AppFog.new(username: 'username', password: 'password', shell_command: self)
 		appfog.start('app-name')
 		assert_equal 'af start app-name', @command
 	end
